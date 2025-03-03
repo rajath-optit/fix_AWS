538161de21be9c56bfd818258966f793680f2004
rajath-optit <128474801+rajath-optit@users.noreply.github.com>
2025-03-03 23:46:57 +0530

    Create Example_report.sh

---
+++ S3/Example_report.sh
@@ -0,0 +1,11 @@
+control_title,status
+EBS snapshots should not be publicly restorable,alarm
+Attached EBS volumes should have encryption enabled,alarm
+EBS volumes should be protected by a backup plan,alarm
+EBS volume encryption at rest should be enabled,alarm
+EBS snapshots should be encrypted,alarm
+EBS encryption by default should be enabled,alarm
+EBS volumes should be attached to EC2 instances,compliant
+EBS volume snapshots should exist,alarm
+EBS volumes should be in a backup plan,alarm
+Attached EBS volumes should have delete on termination enabled,alarm
